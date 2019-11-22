72697e05eebefb0610522172d0ff489e9a4e6516
Afej <afekurojoshua@gmail.com>
2019-11-23 00:53:31 +0100

    first stage changes

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this app</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, minus?
+      </p>
+      <p>version 1</p>
+    </Fragment>
+  );
+};
+
+export default About;
---
+++ src/context/github/githubContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const githubState = createContext();
+
+export default githubState;
